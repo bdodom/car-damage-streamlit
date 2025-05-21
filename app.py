167c7077f799f5c5f97ac879a8432425acda38cf
bdodom <braylondodom@gmail.com>
2025-05-20 20:46:53 -0500

    app.py

--- app.py
+++ app.py
@@ -4,31 +4,6 @@ import torch
 from torchvision import transforms, models
 import torch.nn as nn
 
-# 1. Downloading (car_damage_.pth) - Trained Model Weights
-def download_file_from_google_drive(file_id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-    session = requests.Session()
-    response = session.get(URL, params={'id': file_id}, stream=True)
-    token = None
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            token = value
-    if token:
-        params = {'id': file_id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(32768):
-            if chunk:
-                f.write(chunk)
-
-MODEL_PATH = "car_damage_model.pth"
-GOOGLE_DRIVE_FILE_ID = "1fhFAc0cea3CIvr3dFi0NnZWqkyh_pgM_"
-
-if not os.path.exists(MODEL_PATH):
-    with st.spinner("Downloading model file..."):
-        download_file_from_google_drive(GOOGLE_DRIVE_FILE_ID, MODEL_PATH)
-        st.success("Model downloaded successfully!")
-
 # Define the class names ( Streamlit Script)
 class_names = ['Damaged', 'Whole']
 
